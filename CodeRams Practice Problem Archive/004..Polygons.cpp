bae754c52ef2ef4a1c029b13d685c3318ad03634
Sabiqunnahar <sabiqunnahar65@gmail.com>
2025-05-27 21:09:20 +0600

    Add files via upload

---
+++ CodeRams Practice Problem Archive/004..Polygons.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unsigned long long int t,a;
+    cin>>t;
+
+    while(t--){
+        cin>>a;
+        cout<<(a-2)*180<<endl;
+    }
+    return 0;
+}
+
+
+
