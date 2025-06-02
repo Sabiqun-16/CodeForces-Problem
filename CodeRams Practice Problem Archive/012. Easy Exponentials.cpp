933a5abbb52ac84c5919910a85159f121340e428
Sabiqunnahar <sabiqunnahar65@gmail.com>
2025-06-02 20:00:00 +0600

    Update 012. Easy Exponentials.cpp

--- CodeRams Practice Problem Archive/012. Easy Exponentials.cpp
+++ CodeRams Practice Problem Archive/012. Easy Exponentials.cpp
@@ -1,1 +1,15 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    long d,p;
+    cin>>d>>p;
+
+    long res=1;
+    for(int i=0;i<p;i++) res*=d;
+    cout<<res<<endl;
+
+    return 0;
+}
